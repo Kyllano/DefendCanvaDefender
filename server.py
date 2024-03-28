7c94640b39f29da8e6ac50bbc8d0d9ffde2301af
Kyll <kyll@tutanota.com>
2024-03-28 18:16:17 +0100

    alrighty lois we gud

--- server.py
+++ server.py
@@ -3,14 +3,19 @@ from http.server import SimpleHTTPRequestHandler
 import json
 
 class myHTTPRequestHandler(SimpleHTTPRequestHandler):
+
     def do_GET(self):
+        #self.send_header("Last-Modified", self.date_time_string(time.time()))
+        #self.end_headers()
         return SimpleHTTPRequestHandler.do_GET(self)
+
+        
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length).decode('utf-8') # <--- Gets the data itself
         data = json.loads(post_data)
-        print("coucou", data["noise"])
+        print("coucou", data)
 
         
 
